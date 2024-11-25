e29f737674ff9f9f583380bef53541ff17617f47
alxndrsn <alxndrsn>
2024-11-25 06:23:26 +0000

    test: update test names

--- test.js
+++ test.js
@@ -18,12 +18,12 @@ function getStore({ db }) {
 describe('express-session-better-sqlite3', function() {
   const store = getStore({ db:':memory:' });
 
-  it('should implement createSession()', () => {
+  it('store should implement createSession()', () => {
     // expect
     assert.isFunction(store.createSession);
   });
 
-  it('should save a new session record', done => {
+  it('store should be empty initially', done => {
     store.set('1111222233334444', { cookie:{ maxAge:2000 }, name:'sample name' }, err => {
       if(err) return done(err);
 
@@ -32,7 +32,7 @@ describe('express-session-better-sqlite3', function() {
   });
 
 
-  it('should retrieve an active session', done => {
+  it('get() should retrieve an active session', done => {
     store.get('1111222233334444', (err, session) => {
       if(err) return done(err);
 
@@ -42,7 +42,7 @@ describe('express-session-better-sqlite3', function() {
     });
   });
 
-  it('should gracefully handle retrieving an unkonwn session', done => {
+  it('get() should gracefully handle retrieving an unkonwn session', done => {
     store.get('hope-and-change', err => {
       if(err) return done(err);
 
@@ -50,7 +50,7 @@ describe('express-session-better-sqlite3', function() {
     });
   });
 
-  it('should only contain one session', done => {
+  it('store should only contain one session', done => {
     store.length((err, len) => {
       if(err) return done(err);
 
@@ -61,7 +61,7 @@ describe('express-session-better-sqlite3', function() {
   });
 
   // see: https://github.com/expressjs/session#storesetsid-session-callback
-  it('should support upserting a session', done => {
+  it('set() should support upserting a session', done => {
     store.get('1111222233334444', (err, s1) => {
       if(err) return done(err);
 
@@ -81,7 +81,7 @@ describe('express-session-better-sqlite3', function() {
     });
   });
 
-  it('should clear all session records', done => {
+  it('clear() should delete all session records', done => {
     store.clear(err => {
       if(err) return done(err);
 
@@ -95,7 +95,7 @@ describe('express-session-better-sqlite3', function() {
     });
   });
 
-  it('should destroy a session', function(done) {
+  it('destroy() should delete a session', function(done) {
     store.set('555666777', { cookie:{ maxAge:1000 }, name:'Rob Dobilina' }, err => {
       if(err) return done(err);
 
